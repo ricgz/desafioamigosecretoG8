1f82b16c1d5c631be307382ff3c4b1644f4365b6
Ricardo Gonzalez <geminis_richy@hotmail.com>
2025-01-25 14:46:46 -0300

    Validacion y alerta de nombre en blanco

--- app.js
+++ app.js
@@ -3,13 +3,21 @@
 
 let listadoAmigos = [];
 
+// reglas de validacion de ingreso de nombre de amigos
+function validarIngresoAmigo(nombre){
+    return (nombre == "" ? false : true);
+}
 
 function agregarAmigo() {
     InputAmigo = document.getElementById("amigo");
-    listadoAmigos.push(InputAmigo.value);
 
-    InputAmigo.value = "";
-    InputAmigo.focus();
-    
-    console.log(listadoAmigos);
+    if (validarIngresoAmigo(InputAmigo.value)){
+        listadoAmigos.push(InputAmigo.value);
+        InputAmigo.value = "";
+        InputAmigo.focus();
+        console.log(listadoAmigos);
+    }else{
+        alert("Debe ingresar un nombre valido!");
+        InputAmigo.focus();
+    } 
 }
