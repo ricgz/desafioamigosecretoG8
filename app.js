4f14cc046ad57ab768d93573f23a515cfc447ff5
Ricardo Gonzalez <geminis_richy@hotmail.com>
2025-01-25 14:38:37 -0300

    Se registran los amigos ingresados en la lista listadoAmigos

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. 
+// Aquí deberás desarrollar la lógica para resolver el problema.
+
+let listadoAmigos = [];
+
+
+function agregarAmigo() {
+    InputAmigo = document.getElementById("amigo");
+    listadoAmigos.push(InputAmigo.value);
+
+    InputAmigo.value = "";
+    InputAmigo.focus();
+    
+    console.log(listadoAmigos);
+}
